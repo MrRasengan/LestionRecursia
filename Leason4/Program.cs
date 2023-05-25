e2eedd1e12991299669d002020ff53eaedc16f8b
Konstantin <kostik-kostik-12@mail.ru>
2023-05-25 13:42:45 +0300

    add Leason1

---
+++ Leason4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Рекурсия: числа фибаначи
+// 1 1 2 3 5 8 13 21 ...
+// fn = fn-1 + fn-2
+
+ // f(1) = 1
+ // f(2) = 1
+ // f(n) = f(n-1) + f(n-2)
+
+double Fibanacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else  return Fibanacci(n-1) + Fibanacci(n-2);
+}
+for (int i =1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibanacci(i)}");
+}
